8daa825495503d78981d578c65e2bce6f35cae60
TZ117 <tonyzhou1107@CCA.EDU>
2019-02-23 23:13:01 -0800

    Create spaced-lines.js

---
+++ spaced-lines.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+
+  for (var x = 60; x < width-60; x = x + 30) {
+    line(x, height/2, mouseX, mouseY);
+  }
+}
